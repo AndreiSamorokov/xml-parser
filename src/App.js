3c3b42f213acfb7548a8bd9d86b7f6243a9ddfad
AndreiSamorokov <andreiv0202@gmail.com>
2020-11-05 07:19:08 +0900

    icon install

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Typography from '@material-ui/core/Typography'
 import Appbar from '@material-ui/core/AppBar'
+import Brightness7Icon from '@material-ui/icons/Brightness7';
 
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
   return (
     <div>
       <Appbar>
+        <Brightness7Icon />
         <Typography variant="h5" > Freelancer Feed </Typography>
       </Appbar>
       {/* <button onClick={(e)=>showList()} >Show Job List</button> */}
