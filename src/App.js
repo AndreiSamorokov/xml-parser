9c4bea455b479a4482ade9407eccc02d1e128143
AndreiSamorokov <andreiv0202@gmail.com>
2020-11-05 07:47:08 +0900

    freelancer&material

--- src/App.js
+++ src/App.js
@@ -1,47 +1,69 @@
 import React, { useState } from "react";
 
 import Typography from '@material-ui/core/Typography'
-import Appbar from '@material-ui/core/AppBar'
-import Brightness7Icon from '@material-ui/icons/Brightness7';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Container from '@material-ui/core/Container'
+import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles'
+import { orange } from '@material-ui/core/colors'
+import Brightness7Icon from '@material-ui/icons/Brightness7'
 
 
 function App() {
   
-  const [xmlvalue, setXmlvalue] = useState([]);
+  const [xmlvalue, setXmlvalue] = useState([]); 
+ 
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: orange[500],
+      }
+    }
+  })
+
+  // showList();
   
-  showList();
-  
-  function showList() { 
-    fetch(
-      `https://cors-anywhere.herokuapp.com/https://www.freelancer.com/rss.xml`
-    )
-    .then(response => {
-      return response.text();
-    })
-    .then(res => {
+  // function showList() { 
+  //   fetch(
+  //     `https://cors-anywhere.herokuapp.com/https://www.freelancer.com/rss.xml`
+  //   )
+  //   .then(response => {
+  //     return response.text();
+  //   })
+  //   .then(res => {
       
-      var XMLParser = require("react-xml-parser");
-      var xml = new XMLParser().parseFromString(res); 
-      let items = xml.getElementsByTagName("item"); 
-      setXmlvalue(items); 
+  //     var XMLParser = require("react-xml-parser");
+  //     var xml = new XMLParser().parseFromString(res); 
+  //     let items = xml.getElementsByTagName("item"); 
+  //     setXmlvalue(items); 
 
-    });
+  //   });
 
-  }
+  // }
  
   return (
-    <div>
-      <Appbar>
-        <Brightness7Icon />
-        <Typography variant="h5" > Freelancer Feed </Typography>
-      </Appbar>
-      {/* <button onClick={(e)=>showList()} >Show Job List</button> */}
-      {xmlvalue.map((item, key) => (
-        <ul key={ key }>
-            <li >{ item.children[key].value }</li>
-        </ul> 
-      ))}
-    </div>
+    <ThemeProvider theme={theme}> 
+
+      <Container>
+        <div className="App">
+          <header className="App-header">
+            <AppBar>
+              <Toolbar>
+                <Brightness7Icon />
+                <Typography variant="h5" > Freelancer Feed </Typography>
+              </Toolbar>
+            </AppBar>
+          </header>
+          {/* {xmlvalue.map((item, key) => (
+            <ul key={ key }>
+                <li>{ item.children[key].value }</li>
+            </ul> 
+          ))} */}
+        </div>
+      </Container>
+
+    </ThemeProvider>
   );  
 }
 
